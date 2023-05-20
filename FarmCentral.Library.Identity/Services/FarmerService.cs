1ce306c2e4cac592365f0d0e8fdfb110c00e26b6
Cale Govindasami <ST10083941@vcconnect.edu.za>
2023-05-20 12:31:07 +0200

    Added farmer service.

---
+++ FarmCentral.Library.Identity/Services/FarmerService.cs
@@ -0,0 +1,54 @@
+﻿using FarmCentral.Library.Identity.Models;
+using FarmCentral.Library.Shared.Identity;
+using FarmCentral.Library.Shared.Models.Identity;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmCentral.Library.Identity.Services
+{
+    public class FarmerService : IFarmerService
+    {
+
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public FarmerService(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<Farmer> GetFarmer(string id)
+        {
+            ApplicationUser? user = await _userManager.FindByIdAsync(id);
+            
+            Farmer farmer = new()
+            {
+                Id = user.Id,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                Address = user.Address!
+            };
+            return farmer;
+        }
+
+        public async Task<List<Farmer>> GetFarmers()
+        {
+            IList<ApplicationUser>? users = await _userManager.GetUsersInRoleAsync("Farmer");
+
+            List<Farmer> farmers = users.Select(x => new Farmer()
+            {
+                Id = x.Id,
+                FirstName = x.FirstName,
+                LastName = x.LastName,
+                Email = x.Email,
+                Address = x.Address!
+            }).ToList();
+
+            return farmers;
+            
+        }
+    }
+}
