689ec13b957604b08566334c7a00f6f7c0f3f957
Cale Govindasami <ST10083941@vcconnect.edu.za>
2023-05-28 13:57:27 +0200

    Added product controller.

---
+++ FarmCentral.API.Application/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using FarmCentral.Library.Shared.Contracts.Repository;
+using FarmCentral.Library.Shared.Models.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FarmCentral.API.Application.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpPost("post")]
+        public async Task<ProductDto> Post(ProductDto product)
+        {
+            return await _productRepository.CreateAsync(product);
+        }
+        [HttpDelete("delete")]
+        public async Task<ProductDto> Delete(ProductDto product)
+        {
+            return await _productRepository.DeleteAsync(product);
+        }
+
+        [HttpGet("get")]
+        public async Task<List<ProductDto>> Get()
+        {
+            return await _productRepository.GetAsync();
+        }
+
+        [HttpGet("getbyid")]
+        public async Task<ProductDto> GetById(int id)
+        {
+            return await _productRepository.GetByIdAsync(id);
+        }
+    }
+}
