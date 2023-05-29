6bcdcf4b80775ca1384d8fbadfe102a4a6732b6d
Cale Govindasami <ST10083941@vcconnect.edu.za>
2023-05-29 11:42:59 +0200

    Farmer products refresh when a new one is added.

--- FarmCentral.Library.Shared/Models/DTOs/FarmerProductDto.cs
+++ FarmCentral.Library.Shared/Models/DTOs/FarmerProductDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,15 @@ namespace FarmCentral.Library.Shared.Models.DTOs
     {
         public int FarmerProductId { get; set; }
         public string FarmerId { get; set; } = null!;
+        [Required]
+         
         public decimal PricePerUnit { get; set; }
+        [Required]
         public int Quantity { get; set; }
+        [Required]
+        [MaxLength(64)]
         public string ProductName { get; set; } = null!;
+        [Required]
         public DateTime? DateAdded { get; set; }
     }
 }
