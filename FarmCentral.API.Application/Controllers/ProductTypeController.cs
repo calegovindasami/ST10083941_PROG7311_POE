6cf40c382f383768ba121b395ada918ffc165d06
ST10083941 <96740648+cgov-0406@users.noreply.github.com>
2023-06-23 14:03:04 +0200

    Added product type controller to API.

---
+++ FarmCentral.API.Application/Controllers/ProductTypeController.cs
@@ -0,0 +1,42 @@
+﻿using FarmCentral.Library.Shared.Contracts.Repository;
+using FarmCentral.Library.Shared.Models.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FarmCentral.API.Application.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductTypeController : Controller
+{
+    private readonly IProductTypeRepository _repository;
+
+    public ProductTypeController(IProductTypeRepository repository)
+    {
+        _repository = repository;
+    }
+
+    [HttpPost("post")]
+    public async Task<ProductTypeDto> Post(ProductTypeDto model)
+    {
+        return await _repository.CreateAsync(model);
+    }
+
+    [HttpDelete("delete")]
+    public async Task<ProductTypeDto> Delete(ProductTypeDto model)
+    {
+        return await _repository.DeleteAsync(model);
+    }
+
+    [HttpGet("get")]
+    public async Task<List<ProductTypeDto>> Get()
+    {
+        return await _repository.GetAsync();
+    }
+
+    [HttpGet("getbyid")]
+    public async Task<ProductTypeDto> GetById(int id)
+    {
+        return await _repository.GetByIdAsync(id);
+    }
+
+}
